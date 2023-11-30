ed81bfcc9b234ba036765a862078e8471a3d38c9
Biplab Biswas <biswasbiplab98@gmail.com>
2023-11-30 13:25:08 +0530

    creating url dispatcher for "chatBot_openAI" page

--- apps/views.py
+++ apps/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("This is home page")
+
+def chatBot_openAI(request):
+    return HttpResponse("This is openAI chatbot Page")
